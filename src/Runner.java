fbfbabc9444ce6ea00c86f7e67945a979c646f36
deepvn98 <deepvn988@gmail.com>
2021-04-17 15:32:55 +0700

    baitap

--- src/Runner.java
+++ src/Runner.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 
 public class Runner {
     public static void main(String[] args) {
-
         ArrayList<Book> books = new ArrayList<>();
         ManagerBook tester = new ManagerBook(books);
         tester.menuBook();
